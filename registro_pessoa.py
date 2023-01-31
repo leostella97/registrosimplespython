fd26ee2a71bd5b26ea47e1d4d910094bb1b5925f
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-01-31 11:03:30 -0300

    Add files via upload

---
+++ registro_pessoa.py
@@ -0,0 +1,13 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite a sua idade: ")
+email = input("Digite seu endereço de e-mail: ")
+
+print("Os dados estão corretos?", nome, idade, email, sep="\n")
+resposta = input("Escreva sua resposta com S para SIM e N para NÃO: ")
+
+if (resposta == "S" or resposta == "s"):
+    print("Informações cadastradas com sucesso!")
+elif (resposta == "N" or resposta =="n"):
+    print("As informações não foram incluídas no banco de dados, favor reiniciar o aplicativo.")
+else:
+    print("Por favor, responder com S ou N, reinicie o aplicativo.")
